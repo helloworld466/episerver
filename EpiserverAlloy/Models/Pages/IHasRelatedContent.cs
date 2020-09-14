c1d89e94cf155cc2f0352d82b67df98969a02e54
jananiAM <jananiam1997@gmail.com>
2020-09-14 10:19:56 +0530

    second commit

--- EpiserverAlloy/Models/Pages/IHasRelatedContent.cs
+++
@@ -1,9 +0,0 @@
-using EPiServer.Core;
-
-namespace EpiserverAlloy.Models.Pages
-{
-    public interface IHasRelatedContent
-    {
-        ContentArea RelatedContentArea { get; }
-    }
-}
--- EpiserverAlloy/Models/Pages/ISearchPage.cs
+++
@@ -1,9 +0,0 @@
-namespace EpiserverAlloy.Models.Pages
-{
-    /// <summary>
-    /// Marker interface for search implementation
-    /// </summary>
-    public interface ISearchPage
-    {
-    }
-}
